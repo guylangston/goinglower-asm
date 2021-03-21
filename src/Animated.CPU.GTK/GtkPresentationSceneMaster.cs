47bbbbbb152ddfe15d3ad9d152388983b38321a4
Guy Langston <guylangston@gmail.com>
2021-03-21 18:26:46 +0000

    Better paths

--- src/Animated.CPU.GTK/GtkPresentationSceneMaster.cs
+++ src/Animated.CPU.GTK/GtkPresentationSceneMaster.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Animated.CPU.Animation;
 using Animated.CPU.Backend.LLDB;
 using Animated.CPU.Model;
@@ -17,11 +18,18 @@ namespace Animated.CPU.GTK
         {
             var setup = new Setup();
             var cpu = new Cpu();
+
+            var root = Directory.Exists("/home/guy/repo/cpu.anim") 
+                ? "/home/guy/repo/cpu.anim"
+                : Directory.Exists("c:\\projects\\cpu.anim") 
+                    ? "c:\\projects\\cpu.anim"
+                    : throw new Exception("Root Dir?");
+            
             var cfg = new Setup.Config()
             {
                 StoryId           = "Introduction-ForLoop",
-                BaseFolder        = "/home/guy/repo/cpu.anim/src/Sample/Scripts/Introduction-ForLoop",
-                CompileBaseFolder = "/home/guy/repo/cpu.anim/src/Sample/Scripts"
+                BaseFolder        = Path.Combine(root, "src/Sample/Scripts/Introduction-ForLoop"),
+                CompileBaseFolder = Path.Combine(root, "src/Sample/Scripts")
             };
             setup.InitCpuFromDisk(cfg, cpu);
             return cpu;
